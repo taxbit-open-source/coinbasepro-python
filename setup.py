9b1b39e43b2ec6578404441e29f5957ba2c8c79d
Scott Leonard <scott@taxbit.com>
2019-03-29 13:16:53 -0600

    renamed project for fork

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='cbpro',
+    name='taxbit-cbpro',
     version='1.1.4',
     author='Daniel Paquin',
     author_email='dpaq34@gmail.com',
